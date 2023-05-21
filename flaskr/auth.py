71557abc457ef4d6a79b2dc879640f9c8140ab48
Ubuntu <ubuntu@ip-10-0-1-188.ec2.internal>
2023-05-21 14:09:22 +0000

    Sixth Commit

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -65,26 +65,26 @@ def login():
 
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+        user_id = session.get('user_id')
 
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        if user_id is None:
+            g.user = None
+        else:
+            g.user = get_db().execute(
+                'SELECT * FROM user WHERE id = ?', (user_id,)
+            ).fetchone()
 
 @bp.route('/logout')
 def logout():
-    session.clear()
-    return redirect(url_for('index'))
+        session.clear()
+        return redirect(url_for('index'))
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
+            if g.user is None:
+                return redirect(url_for('auth.login'))
 
-        return view(**kwargs)
+            return view(**kwargs)
 
     return wrapped_view
